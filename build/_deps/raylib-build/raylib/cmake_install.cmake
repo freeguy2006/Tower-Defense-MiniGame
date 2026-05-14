f095eb374224f1b40d46148938fbc0d2a3d30a32
李侑霖 <free20061010@gmail.com>
2026-05-14 20:45:54 +0800

    change the project name

--- build/_deps/raylib-build/raylib/cmake_install.cmake
+++ build/_deps/raylib-build/raylib/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OOP_Raylib_Lab")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TowerDefenseGame")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 int main() {
-    InitWindow(2400, 900, "Raylib OOP");
+    InitWindow(2400, 900, "Tower Defense Game");
     SetTargetFPS(60);
     player p1 = game_factory::create_player({640, 360});
     castle c1 = game_factory::create_castle({50, 700});
